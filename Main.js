749dee1cb807116a34b18692abd5f1e7c661b3ab
Mete <meteecsknr@gmail.com>
2025-09-21 22:16:20 +0100

    Add meta tags, Open Graph settings, and initial React components

---
+++ Main.js
@@ -0,0 +1,10 @@
+function Main() {
+  return (
+    <main>
+      <h2>Reserve a Table</h2>
+      <p>Welcome to Little Lemon! Please select a date and time.</p>
+    </main>
+  );
+}
+
+export default Main;
